d0cae04e9f940d5b51ffdd67c1820565152e0153
BowTiedSwan <86532747+BowTiedSwan@users.noreply.github.com>
2022-02-25 12:44:21 +0000

    Added record mapping function

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -8,6 +8,8 @@ contract Domains {
 // A "mapping" data type to store their user domain names
   mapping(string => address) public domains;
   
+  mapping(string => address) public records;
+  
   constructor() {
     console.log("This is my domain contract. Yihaaw");
   }
@@ -19,6 +21,16 @@ contract Domains {
 // This will give us the domain owners' address
   function getAddress(string calldata name) public view returns (address) {
     return domains[name];
+  }
+
+  function setRecord(string calldate name, string calldata record) public {
+    // Chech if the owner is the transaction sender
+    require(domains[name] == msg.sender);
+    records[name] = record;
+  }
+
+  function getRecord(string calldata name) publci view returns(string memory) {
+    return records[name];
   }
 }
 
